6a6f64ed186b1737cce1025993b7fe0779c542ce
DaniUnnoba <ndavila@comunidad.unnoba.edu.ar>
2024-11-21 16:30:53 -0300

    Update UserRepository.java

--- allmusic/src/main/java/ar/edu/unnoba/poo2024/allmusic/repository/UserRepository.java
+++ allmusic/src/main/java/ar/edu/unnoba/poo2024/allmusic/repository/UserRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    public User findByUsername(String username);
+
     //Optional<User> findByUsername(String username);
 }
